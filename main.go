00506666183ad8efb3aaee7a8c9946be2835c838
vegax87 <vegax87@users.noreply.github.com>
2015-01-15 00:14:57 +0100

    removed init() function

--- main.go
+++ main.go
@@ -14,25 +14,6 @@ import (
 
 var db *sql.DB = setupDB()
 
-func init() {
-
-	db.Exec(`CREATE TABLE IF NOT EXISTS `users` (
-  		`id` int(11) NOT NULL AUTO_INCREMENT,
-  		`user_name` varchar(60) NOT NULL,
-  		`user_email` varchar(60) NOT NULL,
-  		`user_password` varchar(60) NOT NULL,
-  		`user_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  		`user_last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  		PRIMARY KEY (`id`),
-  		UNIQUE KEY `user_name` (`user_name`),
-  		UNIQUE KEY `user_email` (`user_email`)
-		) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;`)
-
-	db.Exec(`INSERT IGNORE INTO users (user_name, user_email, user_password)
-             VALUES ('john', 'john@example.com', 'supersecret');`)
-
-}
-
 func main() {
 
 	defer db.Close()
